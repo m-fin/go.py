c46778730e84df1c02896525941cbe1829a90b16
Matthew Finley <mattjf67@gmail.com>
2020-04-10 14:11:45 -0400

    early stages

--- go.py
+++ go.py
@@ -1,16 +1,19 @@
 class Go:
 
-    BLACK = 'x'
-    WHITE = 'o'
-
     def __init__(self, size):
         self.size = size
         self.board = [['.' for row in range(self.size)] for col in range(self.size)]
-        self.player = BLACK
 
-    def board(self):
+        self.players = {
+            'BLACK' : 'x',
+            'WHITE' : 'o'
+        }
+
+        self.player = self.players['BLACK']
+
+    def print_board(self):
         for row in self.board:
-            for elem in self.board:
+            for elem in row:
                 print(elem + ' ', end='')
             
             print()
@@ -20,17 +23,15 @@ class Go:
         row = int(string[0])
         col = string[1]
  
-        row_coord = row - 1
+        y_coord = row - 1
         #! get rid of 'I'
-        col_coord = col - 65
+        x_coord = ord(col) - 65
+     
+        self.board[self.size - y_coord - 1][x_coord] = self.player
+        self.nextPlayer()
 
-        
-        self.board[row_coord][col_coord] = self.player
-        nextPlayer()
-
-    def nextPlayer():
-        if self.player == BLACK:
-            self.player = WHITE
+    def nextPlayer(self):
+        if self.player == self.players['BLACK']:
+            self.player = self.players['WHITE']
         else:
-            self.player = BLACK
-
+            self.player = self.players['BLACK']
